f928d7e675d2b6d54a1acb239912aa612af814e1
Phillip Cabaniss <pcabaniss93@gmail.com>
2024-02-07 03:30:28 -0600

    Up and running

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
-
+ 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
